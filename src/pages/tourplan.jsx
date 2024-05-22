7bd21c450b8a713e79e866e6a8b81633520f2128
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-05-22 11:04:40 +0600

    tourplan page created

---
+++ src/pages/tourplan.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const tourplan = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, delectus
+        excepturi.
+      </h1>
+    </div>
+  );
+};
+
+export default tourplan;
