44ce65daca5089bd69bf7f768fadc37fe7f2b41d
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-05-22 11:06:51 +0600

    404 error page color combination fixed

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,16 +10,16 @@ const NotFound = () => {
         <Image src={errorgif} id="error" alt="Error" />
         <div>
           <div>
-            <h1 className="text-6xl font-bold text-gray-800 mb-4">
+            <h1 className="text-6xl font-bold text-green-500 mb-4">
               Look like you are lost
             </h1>
-            <p className="text-xl text-gray-600 mb-8 text-center">
+            <p className="text-xl text-red-500 mb-8 text-center">
               the page you are looking for not avaible!
             </p>
           </div>
           <div className="text-center">
             <Link href="/">
-              <button className="px-6 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 transition duration-300">
+              <button className="px-6 py-2 text-white bg-green-500 rounded hover:bg-blue-700 transition duration-300">
                 <p>Go back home</p>
               </button>
             </Link>
