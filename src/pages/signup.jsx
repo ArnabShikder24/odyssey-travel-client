5562ecc718bd531f5d0efe36c9f86c333eea6857
ArnabShikder24 <arnabshikder2001@gmail.com>
2025-02-08 19:21:26 +0600

    sign up api changed

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -56,7 +56,7 @@ const SignUp = () => {
           username: user?.displayName,
           email: user?.email
         };
-        const response = await axios.post('http://localhost:5000/api/v1/user/create', userData);
+        const response = await axios.post('http://localhost:8000/api/user/create', userData);
         console.log('New User Created:', user, response);
         toast.success("New User Created");
       }
