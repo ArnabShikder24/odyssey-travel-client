35c90f83b1c5614d92f0ff2904ef606a221ebede
ArnabShikder24 <arnabshikder2001@gmail.com>
2025-02-16 14:04:19 +0600

    role access feautre implemented

--- src/components/DashLayout.jsx
+++ src/components/DashLayout.jsx
@@ -12,6 +12,7 @@ import {
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import AdminProtectedRoute from './AdminProtectedRoute';
 const { Layout, Menu, theme } = antd;
 const { Header, Content, Footer, Sider } = Layout;
 const items = [
@@ -35,116 +36,118 @@ export default function DashLayout({ children }) {
         token: { colorBgContainer, borderRadiusLG },
       } = theme.useToken();
   return (
-    <Layout hasSider>
-      <Sider
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: 0,
-          top: 0,
-          bottom: 0,
-        }}
-      >
-        <div className="demo-logo-vertical" />
-              {/* <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} /> */}
-              <Link href="/dashboard">
-        <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard" && "bg-purple-400"}`}>
-          <ShopOutlined />
-          <span>Dashboard</span>
-            </div>
-            </Link>
-        <Link href="/dashboard/AddPackages">
-            <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AddPackages" && "bg-purple-400"}`}>
-            <CloudOutlined />
-            <span>Add Packages</span>
-            </div>
-        </Link>
-        <Link href="/dashboard/AllPackages">
-        <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllPackages" && "bg-purple-400"}`}>
-          <ShopOutlined />
-          <span>All Packages</span>
-        </div>
-        </Link>
-        <Link href="/dashboard/AddHotels">
-            <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AddHotels" && "bg-purple-400"}`}>
-            <CloudOutlined />
-            <span>Add Hotels</span>
-            </div>
-        </Link>
-        <Link href="/dashboard/AllHotels">
-        <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllHotels" && "bg-purple-400"}`}>
-          <ShopOutlined />
-          <span>All Hotels</span>
-        </div>
-        </Link>
-        <Link href="/dashboard/AddFlights">
-            <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AddFlights" && "bg-purple-400"}`}>
-            <CloudOutlined />
-            <span>Add Flights</span>
-            </div>
-        </Link>
-        <Link href="/dashboard/AllFlights">
-        <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllFlights" && "bg-purple-400"}`}>
-          <ShopOutlined />
-          <span>All Flights</span>
-        </div>
-        </Link>
-        <Link href="/dashboard/AddGuides">
-            <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AddGuides" && "bg-purple-400"}`}>
-            <CloudOutlined />
-            <span>Add Guides</span>
-            </div>
-        </Link>
-        <Link href="/dashboard/AllGuides">
-        <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllGuides" && "bg-purple-400"}`}>
-          <ShopOutlined />
-          <span>All Guides</span>
-        </div>
-        </Link>
-        <Link href="/dashboard/AllUser">
-        <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllUser" && "bg-purple-400"}`}>
-          <ShopOutlined />
-          <span>All User</span>
-          </div>
-        </Link>
-        <Link href="/dashboard/AllBooking">
-        <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllBooking" && "bg-purple-400"}`}>
-          <ShopOutlined />
-          <span>All Booking</span>
-          </div>
-        </Link>
-      </Sider>
-      <Layout
-        style={{
-          marginLeft: 200,
-        }}
-      >
-        <Content
+    <AdminProtectedRoute>
+      <Layout hasSider>
+        <Sider
           style={{
-            margin: '24px 16px 0',
-            overflow: 'initial',
+            overflow: 'auto',
+            height: '100vh',
+            position: 'fixed',
+            left: 0,
+            top: 0,
+            bottom: 0,
           }}
         >
-          <div
+          <div className="demo-logo-vertical" />
+                {/* <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} /> */}
+                <Link href="/dashboard/main">
+          <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/main" && "bg-purple-400"}`}>
+            <ShopOutlined />
+            <span>Dashboard</span>
+              </div>
+              </Link>
+          <Link href="/dashboard/AddPackages">
+              <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AddPackages" && "bg-purple-400"}`}>
+              <CloudOutlined />
+              <span>Add Packages</span>
+              </div>
+          </Link>
+          <Link href="/dashboard/AllPackages">
+          <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllPackages" && "bg-purple-400"}`}>
+            <ShopOutlined />
+            <span>All Packages</span>
+          </div>
+          </Link>
+          <Link href="/dashboard/AddHotels">
+              <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AddHotels" && "bg-purple-400"}`}>
+              <CloudOutlined />
+              <span>Add Hotels</span>
+              </div>
+          </Link>
+          <Link href="/dashboard/AllHotels">
+          <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllHotels" && "bg-purple-400"}`}>
+            <ShopOutlined />
+            <span>All Hotels</span>
+          </div>
+          </Link>
+          <Link href="/dashboard/AddFlights">
+              <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AddFlights" && "bg-purple-400"}`}>
+              <CloudOutlined />
+              <span>Add Flights</span>
+              </div>
+          </Link>
+          <Link href="/dashboard/AllFlights">
+          <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllFlights" && "bg-purple-400"}`}>
+            <ShopOutlined />
+            <span>All Flights</span>
+          </div>
+          </Link>
+          <Link href="/dashboard/AddGuides">
+              <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AddGuides" && "bg-purple-400"}`}>
+              <CloudOutlined />
+              <span>Add Guides</span>
+              </div>
+          </Link>
+          <Link href="/dashboard/AllGuides">
+          <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllGuides" && "bg-purple-400"}`}>
+            <ShopOutlined />
+            <span>All Guides</span>
+          </div>
+          </Link>
+          <Link href="/dashboard/AllUser">
+          <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllUser" && "bg-purple-400"}`}>
+            <ShopOutlined />
+            <span>All User</span>
+            </div>
+          </Link>
+          <Link href="/dashboard/AllBooking">
+          <div style={{ cursor: 'pointer' }} className={`text-white flex gap-5 px-5 py-3 hover:bg-purple-400 ${pathname === "/dashboard/AllBooking" && "bg-purple-400"}`}>
+            <ShopOutlined />
+            <span>All Booking</span>
+            </div>
+          </Link>
+        </Sider>
+        <Layout
+          style={{
+            marginLeft: 200,
+          }}
+        >
+          <Content
             style={{
-              padding: 24,
-              textAlign: 'center',
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
+              margin: '24px 16px 0',
+              overflow: 'initial',
             }}
           >
-            {children}
-          </div>
-        </Content>
-        <Footer
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          Odyssey ©{new Date().getFullYear()} 
-        </Footer>
+            <div
+              style={{
+                padding: 24,
+                textAlign: 'center',
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              {children}
+            </div>
+          </Content>
+          <Footer
+            style={{
+              textAlign: 'center',
+            }}
+          >
+            Odyssey ©{new Date().getFullYear()} 
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </AdminProtectedRoute>
   );
 }
